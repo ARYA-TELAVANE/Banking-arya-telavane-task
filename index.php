4882d5947d8c1005d59646e868bef51551e52930
ARYA-TELAVANE <arya3.telavane@gmail.com>
2021-06-17 22:14:14 +0530

    arya
    done by arya banking system

---
+++ index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Basic Banking System</title>
+  <style>
+	.footer {
+   height:120px;
+   width: 100%;
+   color: #fff;
+   text-align: center;
+   background-color :#000;
+   padding:10px
+}
+	body
+    {
+     background-image:url('https://images.unsplash.com/photo-1577915207354-8f266a3fc016?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80')
+
+    }
+    .background
+      {
+        height:399px
+      }
+	</style>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+    <div class="container ">
+      <div class="container-fluid">
+      <!-- Introduction section -->
+            <div >
+            <div class="background">
+                <div class="heading text-center my-4">
+                  <br><br><br>
+                  <h3 style=" color:Black ">Welcome to</h3>
+                  <h1 style=" color:Black  ">SPARKS  COOPERATION BANK</h1>
+                  
+                </div>
+              </div>
+            </div>
+			<div class="container-fluid text-centre" >
+       
+			<div class="container-fluid-sm" >
+            
+     </div></div>
+      </div><br><br><br>
+    </div>
+      <div class="footer mt-5">
+        <p> Made by <b>ARYA TELAVANE</b> <br> The Sparks Foundation</p>
+		<p><a href="https://github.com/ARYA-TELAVANE"><i class="fa fa-github" style="font-size:36px"></i></a>
+		<a href="https://www.linkedin.com/in/arya-telavane-5b2436207/"><i class="fa fa-linkedin-square" style="font-size:36px;color:blue"></i></a>
+		</p>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+ </body>
+</html>
